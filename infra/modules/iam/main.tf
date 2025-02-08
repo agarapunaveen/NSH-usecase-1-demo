76406fddbd23063b0ea8fef8e22a35f0071d3491
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-08 16:23:33 +0530

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -91,3 +91,44 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_attach_xray" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
 }
+
+
+# Prometheus Task Role
+resource "aws_iam_role" "prometheus_task_role" {
+  name = "prometheus-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Policy to allow Prometheus to discover ECS targets
+resource "aws_iam_role_policy" "prometheus_ecs_discovery" {
+  name = "prometheus-ecs-discovery"
+  role = aws_iam_role.prometheus_task_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:ListClusters",
+          "ecs:ListTasks",
+          "ecs:DescribeTasks",
+          "ec2:DescribeInstances"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
