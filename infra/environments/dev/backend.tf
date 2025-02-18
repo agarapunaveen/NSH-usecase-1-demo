1c16a12606451c36e56699bb7487b085da52a98a
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-18 13:42:26 +0530

    Update backend.tf

--- infra/environments/dev/backend.tf
+++ infra/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform{
   backend "s3" {
-    bucket         = "52297389-bucket-1"
+    bucket         = "nsh-usecase-1"
     key            = "NSH-fargate/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
