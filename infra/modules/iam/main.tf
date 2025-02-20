43823b682e1ba88aa7d837f9d75d5b02fc2dcce4
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-20 20:59:19 +0530

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -9,7 +9,6 @@ resource "aws_iam_role" "ecs_task_role" {
     }]
   })
 }
-
 resource "aws_iam_role_policy_attachment" "ecs_execution" {
   role = aws_iam_role.ecs_task_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
