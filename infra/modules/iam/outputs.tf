ec528c70c4ca794ad1ca394593f16bf530ea79ab
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-20 21:02:52 +0530

    Update outputs.tf

--- infra/modules/iam/outputs.tf
+++ infra/modules/iam/outputs.tf
@@ -3,10 +3,10 @@ output "ecs_task_role_arn" {
 value = aws_iam_role.ecs_task_role.arn
 
 }
-/*output "ecs_task_execution_role_arn" {
+output "ecs_task_execution_role_arn" {
   value = aws_iam_role.ecs_execution_role.arn
-}*/
+}
 
-/*output "prometheus_role_arn" {
+output "prometheus_role_arn" {
   value = aws_iam_role.prometheus_task_role.arn
-}*/
+}
